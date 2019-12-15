7cf0391209e3d2120c610c915d36d0a5a93af03a
Yingchao Zou <gychzou@outlook.com>
2019-12-15 23:33:00 +0800

    Expanded layout.

---
+++ dicee-flutter-master/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image(
+                image: AssetImage('images/dice1.png'),
+              ),
+              onPressed: () {
+                print('Left button got pressed.');
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image(
+                image: AssetImage('images/dice2.png'),
+              ),
+              onPressed: () {
+                print('Right button got pressed.');
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
